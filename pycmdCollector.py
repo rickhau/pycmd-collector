a61fe8831dc2284938d2978ec75e4ec6a072424e
Rick Lin <rickhau@gmail.com>
2014-11-21 11:33:25 +0800

    Add docstring to every method

--- pycmdCollector.py
+++ pycmdCollector.py
@@ -6,6 +6,7 @@
 # on Windows and Linux platforms.
 #
 # Change History
+# v0.0.3 Add docstring to every method
 # v0.0.2 Add PyWin32 and Windows Event Log dump
 # v0.0.1 Initial Release
 
@@ -19,7 +20,7 @@ import time
 import shutil
 import csv
 
-version = '0.0.2'
+version = '0.0.3'
 author = 'Rick Lin'
 platform = sys.platform
 if 'cygwin' == platform or 'win32' == platform:
@@ -37,37 +38,53 @@ LOGPATH       = os.path.join(os.getcwd(), LOGDIRNAME)
 LOGLIST       = []
 
 
-def createLogDir(DIR=LOGDIRNAME):
+def createLogDir():
+    """This creates a log directory called 'cmdoutput' for log dump.
+       The directory will be deleted after log archive."""
+
+    global LOGPATH
+    global LOGDIRNAME
+
     logging.debug("Entering createLogDir()")
     if os.path.exists(LOGPATH): # if any previous log exists, remove it
         shutil.rmtree(LOGPATH)
-    os.mkdir(DIR) # create a new one
+    os.mkdir(LOGDIRNAME) # create a new one
     os.chdir(LOGPATH)
 
     logging.debug("Switch to log directory: %s " % os.getcwd())
     logging.debug("Exit createLogDir()")
     return
 
-def zipLogDir(path=LOGPATH):
+def zipLogDir():
+    """This creates a zip to archive all logs in 'cmdoutput' directory"""
+
     global ZIPNAME
+    global TIMEFORMAT
+    global LOGPATH
+
     now = time.localtime(time.time())
     date = time.strftime(TIMEFORMAT, now)
     ZIPNAME = date + ZIPFILENAME + '.zip'
     zipf = zipfile.ZipFile(ZIPNAME, 'w')
-    for dirname, subdirs, files in os.walk(path):
+    for dirname, subdirs, files in os.walk(LOGPATH):
         for file in files:
             if file.endswith('log'):
                 zipf.write(file)
     zipf.close()
 
-    #curdir = os.getcwd()
-    os.chdir('..')
+    os.chdir('..') 
     updir = os.getcwd()
-    shutil.move(os.path.join(LOGPATH, ZIPNAME), updir)
+    shutil.move(os.path.join(LOGPATH, ZIPNAME), updir) # move zip file to parent directory
+    logging.debug("%s archive is created at %s " % (ZIPNAME, updir))
+    shutil.rmtree(LOGPATH)
     return
 
 def getstatusoutput(cmd):
-    """Return (status, output) of executing cmd in a shell."""
+    """ This capture the output and status of the command execution.
+        cmd: individual command
+        Return (status, output) of executing cmd in a shell.
+    """
+
     pipe = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)  
     output = "".join(pipe.stdout.readlines()) 
     sts = pipe.returncode
@@ -76,6 +93,11 @@ def getstatusoutput(cmd):
     return sts, output  
 
 def runCommands(cmd, outfile):
+    """ This executes commands and writes the command output to file, which is to do the log dump
+        cmd: individual command
+        outfile: the filename of log dump
+    """
+    
     global LOGLIST
     logging.debug("Entering runCommands()")
     (status, output) = getstatusoutput(cmd)
@@ -92,21 +114,11 @@ def runCommands(cmd, outfile):
         logging.debug("Exit runCommands()")
     return
 
-def rmLogs(path=LOGPATH):
-    global LOGLIST
-    for dirname, subdirs, files in os.walk(path):
-        for file in files:
-            if file in LOGLIST:
-                os.remove(os.path.join(dirname, file))
-                LOGLIST.remove(file)
-    #logging.info("current dir: %s " % (os.getcwd()))
-    if os.path.basename(os.getcwd()) == LOGDIRNAME:
-        parentdir = os.path.dirname(os.getcwd())
-        os.chdir(parentdir)
-    shutil.rmtree(os.path.join(os.getcwd(), LOGDIRNAME))
-    return
-
 def openCommand(cmdfile):
+    """ This parses the command recipe and run it line by line
+        cmdfile: command receipe
+    """
+    
     try:
         with open(cmdfile, 'r') as f:
             lines = csv.reader(f)
@@ -116,18 +128,18 @@ def openCommand(cmdfile):
                 runCommands(line[0].strip(), line[1].strip()) # strip() is to remove spaces
     except IndexError as e:
         logging.info("[{}]: Incorrect csv format for parsing!, {}".format(os.path.basename(cmdfile), e))
-        rmLogs(os.path.dirname(os.getcwd()))
+        shutil.rmtree(os.path.dirname(os.getcwd()))
         sys.exit()
     except(csv.Error, IOError, ValueError) as e:
         logging.info("Error: {}".format(e))
-        rmLogs(os.path.dirname(os.getcwd()))
+        shutil.rmtree(os.path.dirname(os.getcwd()))
         sys.exit()
 
 
 def main():
-    '''function document
-    logging module practice
+    '''This main fuction reads the command recipe, runs it and archives the log dump into zip file
     '''
+
     formatter = "%(module)s.%(funcName)s |%(asctime)-15s|(PID: %(process)d)|%(levelname)s|%(message)s"
 
     if 'DEBUG' == logging_level:
@@ -164,7 +176,7 @@ def main():
             raise SystemExit
     ### Dump Windows Event Log ###
 
-    createLogDir(LOGDIRNAME)
+    createLogDir()
 
     ### Dump Windows Event Log ###
     if os_name == 'Win':
@@ -175,8 +187,7 @@ def main():
     ### Dump Windows Event Log ###
 
     openCommand(cmdlistfile)
-    zipLogDir(LOGPATH)
-    rmLogs(LOGPATH)
+    zipLogDir()
 
     logging.info("\nLog dump arcive is located at [{}]".format(os.path.abspath(ZIPNAME)))
 
